afebc65f9a466d6edf15dd83475b822826c4681d
SampritiKakoty <33862442+SampritiKakoty@users.noreply.github.com>
2017-12-01 15:34:12 +0530

    Updated commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule,Routes} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+
+import { HttpModule } from '@angular/http';
+import { DbserviceService } from './dbservice.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpModule,
+    RouterModule.forRoot([
+      {path: 'home', component : HomeComponent},
+      {path: 'search',component: SearchComponent}
+     // {path: 'quiz/:quesNo',component: SearchComponent},
+   //   {path: 'result',component: ResultsComponent},
+    ])
   ],
-  providers: [],
+  providers: [DbserviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
